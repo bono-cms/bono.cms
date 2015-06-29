30a588cff92f5d910de07e3d58090d3d4a4acd10
David Yang <daworld.ny@gmail.com>
2015-06-30 03:42:37 +0500

    Removed $editing and replaced with getId()

--- module/News/Controller/Admin/Post/Edit.php
+++ module/News/Controller/Admin/Post/Edit.php
@@ -27,7 +27,6 @@ final class Edit extends AbstractPost
 			$this->loadSharedPlugins();
 
 			return $this->view->render($this->getTemplatePath(), $this->getSharedVars(array(
-				'editing' => true,
 				'title' => 'Edit the post',
 				'post' => $post,
 			)));
