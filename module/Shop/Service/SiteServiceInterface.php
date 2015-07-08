203a33c58c4fa462a417b78813df51f0ea3a0d8c
David Yang <daworld.ny@gmail.com>
2015-07-08 10:51:44 +0500

    Added missing SiteServiceInterface

---
+++ module/Shop/Service/SiteServiceInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) 2015 David Yang <daworld.ny@gmail.com>
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Shop\Service;
+
+interface SiteServiceInterface
+{
+	/**
+	 * Returns an array of entities of recent products
+	 * 
+	 * @param string $id Current product id to be excluded
+	 * @return array
+	 */
+	public function getRecentProducts($id);
+
+	/**
+	 * Returns an array of latest product entities
+	 * 
+	 * @return array
+	 */
+	public function getLatest();
+}
