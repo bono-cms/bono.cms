0b7c8649d932c9cc2606042ef106a7eafc2cba9b
David Yang <daworld.ny@gmail.com>
2015-07-27 11:26:24 +0500

    Minor fixes

--- module/MailForm/Controller/Form.php
+++ module/MailForm/Controller/Form.php
@@ -13,70 +13,11 @@ namespace MailForm\Controller;
 
 use Site\Controller\AbstractController;
 use Krystal\Validate\Pattern;
+use Krystal\Stdlib\VirtualEntity;
 use RuntimeException;
 
 final class Form extends AbstractController
 {
-	/**
-	 * Shows a form
-	 * 
-	 * @param string $id Form id
-	 * @return string
-	 */
-	public function indexAction($id)
-	{
-		if ($this->request->isPost()) {
-			return $this->sendAction($id);
-
-		} else {
-			return $this->showAction($id);
-		}
-	}
-
-	/**
-	 * Shows a form
-	 * 
-	 * @param string $id Form's id
-	 * @return string
-	 */
-	private function showAction($id)
-	{
-		$form = $this->getFormManager()->fetchById($id);
-
-		if ($form !== false) {
-			$this->loadPlugins($form);
-
-			return $this->view->render($form->getTemplate(), array(
-				'page' => $form
-			));
-
-		} else {
-
-			// Returning false will trigger 404 error automatically
-			return false;
-		}
-	}
-
-	/**
-	 * Loads all required plugins for the template
-	 * 
-	 * @param $form
-	 * @return void
-	 */
-	private function loadPlugins($form)
-	{
-		$this->loadSitePlugins();
-
-		// Append handler for a site
-		$this->view->getPluginBag()->appendScript($this->getWithAssetPath('/form.js'));
-		$this->view->getBreadcrumbBag()->add(array(
-			array(
-				'link' => '#',
-				'name' => $form->getTitle()
-			)
-		));
-	}
-
 	/**
 	 * Returns a list of validation rules for dynamic forms
 	 * 
@@ -98,6 +39,100 @@ final class Form extends AbstractController
 				)
 			)
 		);
+	}
+
+	/**
+	 * Shows a form
+	 * 
+	 * @param string $id Form id
+	 * @return string
+	 */
+	public function indexAction($id)
+	{
+		if ($this->request->isPost()) {
+			return $this->sendAction($id);
+
+		} else {
+			return $this->showAction($id);
+		}
+	}
+
+	/**
+	 * Shows a form
+	 * 
+	 * @param string $id Form id
+	 * @return string
+	 */
+	private function showAction($id)
+	{
+		$form = $this->getFormManager()->fetchById($id);
+
+		if ($form !== false) {
+			$this->loadPlugins($form);
+
+			return $this->view->render($form->getTemplate(), array(
+				'page' => $form
+			));
+
+		} else {
+
+			// Returning false will trigger 404 error automatically
+			return false;
+		}
+	}
+
+	/**
+	 * Sends a form
+	 * 
+	 * @param string $id Form id
+	 * @return string
+	 */
+	private function sendAction($id)
+	{
+		$formValidator = $this->getValidator($id, $this->request->getPost());
+
+		if ($formValidator->isValid()) {
+
+			// Here need to send a message
+			if ($this->getFormManager()->send($this->request->getPost())) {
+
+				$flashKey = 'success';
+				$flashMessage = 'Your message has been sent';
+
+			} else {
+				
+				$flashKey = 'warning';
+				$flashMessage = 'Could not send your message. Please again try later';
+			}
+
+			$this->flashMessenger->set($flashKey, $flashMessage);
+
+			return '1';
+
+		} else {
+
+			return $formValidator->getErrors();
+		}
+	}
+
+	/**
+	 * Loads all required plugins for the template
+	 * 
+	 * @param \Krystal\Stdlib\VirtualEntity $form
+	 * @return void
+	 */
+	private function loadPlugins(VirtualEntity $form)
+	{
+		$this->loadSitePlugins();
+
+		// Append handler for a site
+		$this->view->getPluginBag()->appendScript($this->getWithAssetPath('/form.js'));
+		$this->view->getBreadcrumbBag()->add(array(
+			array(
+				'link' => '#',
+				'name' => $form->getTitle()
+			)
+		));
 	}
 
 	/**
@@ -129,39 +164,5 @@ final class Form extends AbstractController
 	private function getFormManager()
 	{
 		return $this->getModuleService('formManager');
-	}
-
-	/**
-	 * Sends a form
-	 * 
-	 * @param string $id Form id
-	 * @return string
-	 */
-	public function sendAction($id)
-	{
-		$formValidator = $this->getValidator($id, $this->request->getPost());
-
-		if ($formValidator->isValid()) {
-
-			// Here need to send a message
-			if ($this->getFormManager()->send($this->request->getPost())) {
-
-				$flashKey = 'success';
-				$flashMessage = 'Your message has been sent';
-
-			} else {
-				
-				$flashKey = 'warning';
-				$flashMessage = 'Could not send your message. Please again try later';
-			}
-
-			$this->flashMessenger->set($flashKey, $flashMessage);
-
-			return '1';
-
-		} else {
-
-			return $formValidator->getErrors();
-		}
 	}
 }
