40f3ddc476004d5be59c370f99e5f0b6e35503bd
David Yang <daworld.ny@gmail.com>
2015-08-19 15:34:26 +0500

    Cleans in the configuration

--- module/Site/View/Template/bono-blog/theme.config.php
+++ module/Site/View/Template/bono-blog/theme.config.php
@@ -12,25 +12,26 @@
 use Menu\View\BootstrapDropdown;
 
 return array(
+
 	// Menu classes configuration
 	'menu' => array(
-		// Top menu
 		'top' => new BootstrapDropdown(),
-		//'nav' => new Dropdown(),
-		//'partners' => new Dropdown(),
 	),
 	
-	'plugins' => array('to-top'),
+	'plugins' => array(
+		'jquery',
+		'to-top',
+		'bootstrap.core'
+	),
 	
 	// Stylesheets and scripts
 	'theme' => array(
 		'stylesheets' => array(
-			'/css/cosmo.min.css',
-			'/css/blog.css',
+			'css/cosmo.min.css',
+			'css/blog.css',
 		),
 		'scripts' => array(
-			'/js/jquery.min.js',
-			'/js/bootstrap.min.js',
+			// No additional scripts required for this theme
 		)
 	)
 );
