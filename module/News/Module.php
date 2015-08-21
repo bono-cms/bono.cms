9dd8c83515d720c7ff8a667fe44b4fc6f25d245c
David Yang <daworld.ny@gmail.com>
2015-08-21 12:08:26 +0500

    Deprecated factory for image manager

--- module/News/Module.php
+++ module/News/Module.php
@@ -79,13 +79,34 @@ final class Module extends AbstractCmsModule
 	}
 
 	/**
-	 * Returns prepared and configured image manager
+	 * Returns prepared and configured image manager service
 	 * 
 	 * @return \Krystal\Image\Tool\ImageManager
 	 */
 	private function getImageManager()
 	{
-		$factory = new PostImageManagerFactory($this->getAppConfig(), $this->getConfigProvider()->getEntity());
-		return $factory->build();
+		// Grab configuration entity
+		$config = $this->getConfigProvider()->getEntity();
+
+		$options = array(
+			'thumb' => array(
+				'quality' => $config->getCoverQuality(),
+				'dimensions' => array(
+					// For administration panel
+					array(200, 200),
+
+					// Dimensions for the site
+					array($config->getCoverWidth(), $config->getCoverHeight()),
+					array($config->getThumbWidth(), $config->getThumbHeight()),
+				)
+			)
+		);
+
+		return new ImageManager(
+			'/data/uploads/module/news/posts/',
+			$this->appConfig->getRootDir(),
+			$this->appConfig->getRootUrl(),
+			$options
+		);
 	}
 }
