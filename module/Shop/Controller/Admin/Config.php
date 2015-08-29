c3c5d6781ca15271b82c3a4eabfbc4063386b6be
David Yang <daworld.ny@gmail.com>
2015-08-29 18:29:12 +0500

    Updated configuration controller

--- module/Shop/Controller/Admin/Config.php
+++ module/Shop/Controller/Admin/Config.php
@@ -11,104 +11,27 @@
 
 namespace Shop\Controller\Admin;
 
-use Cms\Controller\Admin\AbstractController;
+use Cms\Controller\Admin\AbstractConfigController;
 use Krystal\Validate\Pattern;
 
-final class Config extends AbstractController
+final class Config extends AbstractConfigController
 {
 	/**
-	 * Shows a form
-	 * 
-	 * @return string
+	 * {@inheritDoc}
 	 */
-	public function indexAction()
+	protected function getValidationRules()
 	{
-		$this->loadPlugins();
-
-		return $this->view->render('config', array(
-			'title' => 'Tweaks',
-			'config' => $this->getConfigManager()->getEntity(),
-		));
-	}
-
-	/**
-	 * Saves the form
-	 * 
-	 * @return string
-	 */
-	public function saveAction()
-	{
-		$formValidator = $this->getValidator($this->request->getPost('config'));
-
-		if ($formValidator->isValid()) {
-
-			$this->getConfigManager()->write($this->request->getPost('config'));
-			$this->flashBag->set('success', "Shop's settings have been updated successfully");
-
-			return '1';
-
-		} else {
-			return $formValidator->getErrors();
-		}
-	}
-
-	/**
-	 * Loads required plugins
-	 * 
-	 * @return void
-	 */
-	private function loadPlugins()
-	{
-		$this->view->getPluginBag()
-				   ->appendScript($this->getWithAssetPath('/admin/config.js'));
-
-		$this->view->getBreadcrumbBag()->add(array(
-			array(
-				'name' => 'Shop',
-				'link' => 'Shop:Admin:Browser@indexAction'
-			),
-			array(
-				'name' => 'Configuration',
-				'link' => '#'
-			)
-		));
-	}
-
-	/**
-	 * Returns configuration manager
-	 * 
-	 * @return \Shop\Service\ConfigManager
-	 */
-	private function getConfigManager()
-	{
-		return $this->getModuleService('configManager');
-	}
-
-	/**
-	 * Returns prepared form validator
-	 * 
-	 * @param array $input Raw input data
-	 * @return \Krystal\Validate\ValidatorChain
-	 */
-	private function getValidator(array $input)
-	{
-		return $this->validatorFactory->build(array(
-			'input' => array(
-				'source' => $input,
-				'definition' => array(
-					// Grab these rules from framework's patterns
-					'default_category_per_page_count' => new Pattern\PerPageCount(),
-					'currency' => new Pattern\Currency(),
-					'showcase_count' => new Pattern\PerPageCount(),
-					'category_cover_height' => new Pattern\ImageHeight(),
-					'category_cover_width' => new Pattern\ImageWidth(),
-					'cover_width' => new Pattern\ImageWidth(),
-					'cover_height' => new Pattern\ImageHeight(),
-					'thumb_height' => new Pattern\ImageHeight(),
-					'thumb_width' => new Pattern\ImageWidth(),
-					
-				)
-			)
-		));
+		return array(
+			// Grab these rules from framework's patterns
+			'default_category_per_page_count' => new Pattern\PerPageCount(),
+			'currency' => new Pattern\Currency(),
+			'showcase_count' => new Pattern\PerPageCount(),
+			'category_cover_height' => new Pattern\ImageHeight(),
+			'category_cover_width' => new Pattern\ImageWidth(),
+			'cover_width' => new Pattern\ImageWidth(),
+			'cover_height' => new Pattern\ImageHeight(),
+			'thumb_height' => new Pattern\ImageHeight(),
+			'thumb_width' => new Pattern\ImageWidth(),
+		);
 	}
 }
