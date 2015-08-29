96bee86b258ecb8d21a2458cb77ea45ceb17a156
David Yang <daworld.ny@gmail.com>
2015-08-29 18:22:56 +0500

    Updated configuration

--- module/Search/Controller/Admin/Config.php
+++ module/Search/Controller/Admin/Config.php
@@ -11,89 +11,18 @@
 
 namespace Search\Controller\Admin;
 
-use Cms\Controller\Admin\AbstractController;
+use Cms\Controller\Admin\AbstractConfigController;
 use Krystal\Validate\Pattern;
 
-final class Config extends AbstractController
+final class Config extends AbstractConfigController
 {
 	/**
-	 * Shows configuration form
-	 * 
-	 * @return string
+	 * {@inheritDoc}
 	 */
-	public function indexAction()
+	protected function getValidationRules()
 	{
-		$this->loadPlugins();
-		
-		return $this->view->render('config', array(
-			'title' => 'Search',
-			'config' => $this->getConfigManager()->getEntity()
-		));
-	}
-
-	/**
-	 * Saves settings
-	 * 
-	 * @return string
-	 */
-	public function saveAction()
-	{
-		$formValdator = $this->getValidator($this->request->getPost());
-
-		if ($formValdator->isValid()) {
-
-			$this->getConfigManager()->write($this->request->getPost());
-			$this->flashBag->set('success', 'Settings have been saved');
-
-			return '1';
-
-		} else {
-			return $formValdator->getErrors();
-		}
-	}
-
-	/**
-	 * Loads required plugins
-	 * 
-	 * @return void
-	 */
-	private function loadPlugins()
-	{
-		$this->view->getBreadcrumbBag()->add(array(
-			array(
-				'name' => 'Search',
-				'link' => '#'
-			)
-		));
-
-		$this->view->getPluginBag()->appendScript($this->getWithAssetPath('/admin/config.js'));
-	}
-
-	/**
-	 * Returns configuration manager
-	 * 
-	 * @return \Search\Service\ConfigManager
-	 */
-	private function getConfigManager()
-	{
-		return $this->moduleManager->getModule('Search')->getService('configManager');
-	}
-	
-	/**
-	 * Returns prepared form validator
-	 * 
-	 * @param array $input Raw input data
-	 * @return \Krystal\Validate\ValidatorChain
-	 */
-	private function getValidator(array $input)
-	{
-		return $this->validatorFactory->build(array(
-			'input' => array(
-				'source' => $input,
-				'definition' => array(
-					'per_page_count' => new Pattern\PerPageCount(),
-				)
-			)
-		));
+		return array(
+			'per_page_count' => new Pattern\PerPageCount(),
+		);
 	}
 }
