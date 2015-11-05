07ed775e9db2996a9d6748ab13a080d564059ac4
David Yang <daworld.ny@gmail.com>
2015-11-05 11:47:59 +0500

    Code clean-up in installation

--- module/Cms/Controller/Install/AbstractInstallController.php
+++ module/Cms/Controller/Install/AbstractInstallController.php
@@ -16,86 +16,72 @@ use Krystal\Application\View\Resolver\Module as Resolver;
 
 abstract class AbstractInstallController extends AbstractController
 {
-	/**
-	 * {@inheritDoc}
-	 */
-	protected function getResolverThemeName()
-	{
-		return 'install';
-	}
+    /**
+     * {@inheritDoc}
+     */
+    protected function getResolverThemeName()
+    {
+        return 'install';
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	final protected function bootstrap()
-	{
-		$this->view->getBlockBag()->setBlocksDir($this->appConfig->getModulesDir() . '/Cms/View/Template/install/blocks/');
-		$this->view->setLayout('layout', 'Cms');
+    /**
+     * {@inheritDoc}
+     */
+    final protected function bootstrap()
+    {
+        $this->view->getBlockBag()->setBlocksDir($this->appConfig->getModulesDir() . '/Cms/View/Template/install/blocks/');
+        $this->view->setLayout('layout', 'Cms');
 
-		$this->view->getPluginBag()->load(array(
-			'jquery',
-			'bootstrap.core',
-			'bootstrap.cosmo',
-			'admin'
-		));
-	}
+        $this->view->getPluginBag()->load(array(
+            'jquery',
+            'bootstrap.core',
+            'bootstrap.cosmo',
+            'admin'
+        ));
+    }
 
-	/**
-	 * Connects and creates required tables
-	 * 
-	 * @return boolean
-	 */
-	final protected function processAll()
-	{
-		//@TODO
-		return false;
-	}
-
-	/**
-	 * Returns prepared form validator
-	 * 
-	 * @param array $input
-	 * @return \Krystal\Validate\ValidatorChain
-	 */
-	final protected function getValidator(array $input)
-	{
-		return $this->validatorFactory->build(array(
-			'input' => array(
-				'source' => $input,
-				'definition' => array(
-					'host' => array(
-						'required' => true,
-						'rules' => array(
-							'NotEmpty' => array(
-								'message' => 'Database host cannot be empty'
-							)
-						)
-					),
-					
-					'name' => array(
-						'required' => true,
-						'rules' => array(
-							'NotEmpty' => array(
-								'message' => 'Database name cannot be empty'
-							)
-						)
-					),
-					
-					'user' => array(
-						'required' => true,
-						'rules' => array(
-							'NotEmpty' => array(
-								'message' => 'Username cannot be empty'
-							)
-						)
-					),
-					
-					'password' => array(
-						'required' => false,
-						'rules' => array()
-					)
-				)
-			)
-		));
-	}	
+    /**
+     * Returns prepared form validator
+     * 
+     * @param array $input
+     * @return \Krystal\Validate\ValidatorChain
+     */
+    final protected function getValidator(array $input)
+    {
+        return $this->validatorFactory->build(array(
+            'input' => array(
+                'source' => $input,
+                'definition' => array(
+                    'host' => array(
+                        'required' => true,
+                        'rules' => array(
+                            'NotEmpty' => array(
+                                'message' => 'Database host cannot be empty'
+                            )
+                        )
+                    ),
+                    'dbname' => array(
+                        'required' => true,
+                        'rules' => array(
+                            'NotEmpty' => array(
+                                'message' => 'Database name cannot be empty'
+                            )
+                        )
+                    ),
+                    'username' => array(
+                        'required' => true,
+                        'rules' => array(
+                            'NotEmpty' => array(
+                                'message' => 'Username cannot be empty'
+                            )
+                        )
+                    ),
+                    'password' => array(
+                        'required' => false,
+                        'rules' => array()
+                    )
+                )
+            )
+        ));
+    }
 }
--- module/Cms/Controller/Install/Install.php
+++ module/Cms/Controller/Install/Install.php
@@ -11,58 +11,66 @@
 
 namespace Cms\Controller\Install;
 
+use Cms\Install\StorageInstaller;
+
 final class Install extends AbstractInstallController
 {
-	/**
-	 * Renders wizard's page
-	 * 
-	 * @return string
-	 */
-	public function indexAction()
-	{
-		$this->loadPlugins();
+    /**
+     * Renders wizard's page
+     * 
+     * @return string
+     */
+    public function indexAction()
+    {
+        $this->loadPlugins();
 
-		return $this->view->render('install', array(
-			'title' => 'Bono CMS: Installation wizard'
-		));
-	}
+        return $this->view->render('install', array(
+            'title' => 'Bono CMS: Installation wizard'
+        ));
+    }
 
-	/**
-	 * Installs the system
-	 * 
-	 * @return string
-	 */
-	public function installAction()
-	{
-		if ($this->request->hasPost('db')) {
-			$formValidator = $this->getValidator($this->request->getPost('db'));
+    /**
+     * Installs the system
+     * 
+     * @return string
+     */
+    public function installAction()
+    {
+        if ($this->request->hasPost('db')) {
 
-			if ($formValidator->isValid()) {
-				$result = $this->processAll();
+            $input = $this->request->getPost('db');
+            $formValidator = $this->getValidator($input);
 
-				if (!$result) {
-					return $this->translator->translate('Cannot connect to database server. Make sure the data is valid!');
-				} else {
-					return '1';
-				}
+            if ($formValidator->isValid()) {
 
-			} else {
-				return $formValidator->getErrors();
-			}
-		}
-	}
+                $installer = new StorageInstaller();
+                $result = $installer->install('db.config.php', $input);
 
-	/**
-	 * Loads required plugins for template view
-	 * 
-	 * @return void
-	 */
-	private function loadPlugins()
-	{
-		$this->view->getPluginBag()
-				   ->appendScript('@Cms/install.js');
+                $result = $this->processAll($input);
 
-		$this->view->getBlockBag()
-				   ->setBlocksDir($this->getWithViewPath('/blocks/', 'Cms', 'admin'));
-	}
+                if (!$result) {
+                    return $this->translator->translate('Cannot connect to database server. Make sure the data is valid!');
+                } else {
+                    return '1';
+                }
+
+            } else {
+                return $formValidator->getErrors();
+            }
+        }
+    }
+
+    /**
+     * Loads required plugins for template view
+     * 
+     * @return void
+     */
+    private function loadPlugins()
+    {
+        $this->view->getPluginBag()
+                   ->appendScript('@Cms/install.js');
+
+        $this->view->getBlockBag()
+                   ->setBlocksDir($this->getWithViewPath('/blocks/', 'Cms', 'admin'));
+    }
 }
