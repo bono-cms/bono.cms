3ad5056c51681eb9c6b54d210f5b54254bb05c2f
David Yang <daworld.ny@gmail.com>
2016-05-05 23:43:27 +0500

    Added basic module handler

---
+++ module/Cms/Controller/Admin/ModuleManager.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Cms\Controller\Admin;
+
+final class ModuleManager extends AbstractController
+{
+    /**
+     * Renders a grid of modules
+     * 
+     * @return string
+     */
+    public function indexAction()
+    {
+        $modules = array();
+        $current = $this->moduleManager->getLoadedModules();
+
+        foreach ($current as $module) {
+            if ($module->hasConfig('module')) {
+                $modules[] = $module->getConfig('module');
+            }
+        }
+
+        $this->view->getBreadcrumbBag()
+                   ->addOne('Module manager');
+
+        return $this->view->render('module-manager', array(
+            'modules' => $modules
+        ));
+    }
+}
