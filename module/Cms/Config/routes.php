09ad1dfe192884a9686896a763e0bfa60e34fa8f
David Yang <daworld.ny@gmail.com>
2016-05-05 23:41:24 +0500

    Replaced static admin segment with its placeholder

--- module/Cms/Config/routes.php
+++ module/Cms/Config/routes.php
@@ -14,164 +14,164 @@ return array(
         'controller' => 'Install:Install@readyAction'
     ),
     
-    '/admin'=> array(
+    '/%s'=> array(
         'controller' => 'Admin:Dashboard@indexAction',
     ),
     
-    '/admin/kernel/install-module.ajax' => array(
+    '/%s/kernel/install-module.ajax' => array(
         'controller' => 'Admin:Dashboard@installModuleAction'
     ),
     
-    '/admin/kernel/generate-slug' => array(
+    '/%s/kernel/generate-slug' => array(
         'controller' => 'Admin:Dashboard@slugAction'
     ),
     
-    '/admin/kernel/mode-change' => array(
+    '/%s/kernel/mode-change' => array(
         'controller' => 'Admin:Dashboard@changeModeAction'
     ),
     
-    '/admin/kernel/theme-change' => array(
+    '/%s/kernel/theme-change' => array(
         'controller' => 'Admin:Dashboard@changeThemeAction'
     ),
     
-    '/admin/kernel/items-per-page'  => array(
+    '/%s/kernel/items-per-page'  => array(
         'controller' => 'Admin:Dashboard@itemsPerPageChangeAction'
     ),
     
-    '/admin/login' => array(
+    '/%s/login' => array(
         'controller' => 'Admin:Auth@indexAction'
     ),
     
-    '/admin/login.ajax' => array(
+    '/%s/login.ajax' => array(
         'controller' => 'Admin:Auth@loginAction'
     ),
     
-    '/admin/logout' =>  array(
+    '/%s/logout' =>  array(
         'controller' => 'Admin:Auth@logoutAction'
     ),
     
     // Tweaks
-    '/admin/tweaks' => array(
+    '/%s/tweaks' => array(
         'controller' => 'Admin:Tweaks@indexAction',
         'disallow' => array('guest', 'user')
     ),
     
-    '/admin/tweaks.ajax' => array(
+    '/%s/tweaks.ajax' => array(
         'controller' => 'Admin:Tweaks@saveAction',
         'disallow' => array('guest', 'user')
     ),
     
     
     // Users
-    '/admin/users' => array(
+    '/%s/users' => array(
         'controller' => 'Admin:Users@gridAction',
         'disallow' => array('guest', 'user')
     ),
     
-    '/admin/users/add' => array(
+    '/%s/users/add' => array(
         'controller' => 'Admin:Users@addAction',
         'disallow' => array('guest', 'user')
     ),
     
-    '/admin/users/edit/(:var)' => array(
+    '/%s/users/edit/(:var)' => array(
         'controller'    => 'Admin:Users@editAction',
         'disallow' => array('guest')
     ),
     
-    '/admin/users/save' => array(
+    '/%s/users/save' => array(
         'controller'    => 'Admin:Users@saveAction',
         'disallow' => array('guest', 'user')
     ),
     
-    '/admin/users/delete/(:var)' => array(
+    '/%s/users/delete/(:var)' => array(
         'controller' => 'Admin:Users@deleteAction',
         'ajax' => true,
         'method' => 'POST'
     ),
     
     // Languages
-    '/admin/languages' => array(
+    '/%s/languages' => array(
         'controller' => 'Admin:Languages@gridAction',
         'disallow' => array('guest', 'user')
     ),
     
-    '/admin/languages/add' => array(
+    '/%s/languages/add' => array(
         'controller' => 'Admin:Languages@addAction',
         'disallow' => array('guest', 'user')
     ),
 
-    '/admin/languages/edit/(:var)' => array(
+    '/%s/languages/edit/(:var)' => array(
         'controller' => 'Admin:Languages@editAction',
         'disallow' => array('guest', 'user')
     ),
 
-    '/admin/languages/save' => array(
+    '/%s/languages/save' => array(
         'controller' => 'Admin:Languages@saveAction',
         'disallow' => array('guest', 'user')
     ),
     
-    '/admin/languages/delete/(:var)' => array(
+    '/%s/languages/delete/(:var)' => array(
         'controller' => 'Admin:Languages@deleteAction',
         'disallow' => array('guest', 'user')
     ),
     
-    '/admin/languages/tweak' => array(
+    '/%s/languages/tweak' => array(
         'controller' => 'Admin:Languages@tweakAction',
         'disallow' => array('guest', 'user')
     ),
     
-    '/admin/languages/change.ajax' => array(
+    '/%s/languages/change.ajax' => array(
         'controller' => 'Admin:Languages@changeAction'
     ),
     
     // Notifications
-    '/admin/notifications' => array(
+    '/%s/notifications' => array(
         'controller' => 'Admin:Notifications@indexAction'
     ),
     
-    '/admin/notifications/page/(:var)' => array(
+    '/%s/notifications/page/(:var)' => array(
         'controller' => 'Admin:Notifications@indexAction'
     ),
     
-    '/admin/notifications/delete/(:var)' => array(
+    '/%s/notifications/delete/(:var)' => array(
         'controller' => 'Admin:Notifications@deleteAction'
     ),
     
-    '/admin/notifications/clear' => array(
+    '/%s/notifications/clear' => array(
         'controller' => 'Admin:Notifications@clearAction'
     ),
     
     // Info
-    '/admin/info' => array(
+    '/%s/info' => array(
         'controller' => 'Admin:Info@indexAction',
         'disallow' => array('guest', 'user')
     ),
     
     // History
-    '/admin/history/clear' => array(
+    '/%s/history/clear' => array(
         'controller' => 'Admin:History@clearAction',
         'disallow' => array('user')
     ),
     
-    '/admin/history' => array(
+    '/%s/history' => array(
         'controller' => 'Admin:History@indexAction'
     ),
     
-    '/admin/history/page/(:var)' => array(
+    '/%s/history/page/(:var)' => array(
         'controller' => 'Admin:History@indexAction'
     ),
     
     // Notepad
-    '/admin/notepad' => array(
+    '/%s/notepad' => array(
         'controller' => 'Admin:Notepad@indexAction'
     ),
     
-    '/admin/notepad/save' => array(
+    '/%s/notepad/save' => array(
         'controller' => 'Admin:Notepad@saveAction',
         'disallow' => array('guest')
     ),
     
-    '/admin/module-manager' => array(
+    '/%s/module-manager' => array(
         'controller' => 'Admin:ModuleManager@indexAction',
         'disallow' => array('guest')
     )
