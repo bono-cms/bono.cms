22b8e72715c026f78dee87aa570ed7ec8b246a16
David Yang <daworld.ny@gmail.com>
2016-07-11 23:34:51 +0500

    Switch to shared partials in landing theme

--- module/Site/View/Template/bono-landing/theme.config.php
+++ module/Site/View/Template/bono-landing/theme.config.php
@@ -17,23 +17,28 @@ return array(
 	'menu' => array(
 		'top' => new BootstrapDropdown(),
 	),
-	
+    
 	'plugins' => array(
 		// No plugins loaded for this theme
 	),
-	
+    
 	// Stylesheets and scripts
 	'theme' => array(
 		'stylesheets' => array(
-			//
+			'/css/bootstrap.min.css',
+            'http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800',
+            'http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic',
+            '/font-awesome/css/font-awesome.min.css',
+            '/css/animate.min.css',
+            '/css/creative.css'
 		),
 		'scripts' => array(
-			'js/jquery.js',
-			'js/bootstrap.min.js',
-			'js/jquery.easing.min.js',
-			'js/jquery.fittext.js',
-			'js/wow.min.js',
-			'js/creative.js'
+			'/js/jquery.js',
+			'/js/bootstrap.min.js',
+			'/js/jquery.easing.min.js',
+			'/js/jquery.fittext.js',
+			'/js/wow.min.js',
+			'/js/creative.js'
 		)
 	)
 );
