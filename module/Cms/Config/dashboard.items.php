1a1c197a165833f78a1c0a1e6dd9edac22fa7edf
David Yang <daworld.ny@gmail.com>
2016-11-19 23:20:23 +0500

    Make icons of control panel "colorful"

--- module/Cms/Config/dashboard.items.php
+++ module/Cms/Config/dashboard.items.php
@@ -12,6 +12,7 @@
 return array(
     array(
         'route' => 'Cms:Admin:History@indexAction',
+        'color' => 'slategray',
         'icon' => 'fa fa-history fa-5x',
         'name' => 'History',
         'description' => 'History of latest actions'
@@ -19,12 +20,14 @@ return array(
     array(
         'route' => 'Cms:Admin:Notepad@indexAction',
         'icon' => 'fa fa-sticky-note-o fa-5x',
+        'color' => 'blueviolet',
         'name' => 'Notepad',
         'description' => 'Keep your notes using notepad'
     ),
     array(
         'route' => 'Cms:Admin:Notifications@indexAction',
         'icon' => 'fa fa-child fa-5x',
+        'color' => 'brown',
         'name' => 'Notifications',
         'description' => 'All system notifications'
     ),
