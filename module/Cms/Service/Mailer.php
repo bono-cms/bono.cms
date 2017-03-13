24fb756bec8076b6946476cb09bc509071313472
David Yang <daworld.ny@gmail.com>
2017-03-13 20:19:39 +0500

    Added sendTo()

--- module/Cms/Service/Mailer.php
+++ module/Cms/Service/Mailer.php
@@ -43,19 +43,18 @@ final class Mailer implements MailerInterface
     }
 
     /**
-     * Sends a mail
+     * Sends a message. For internal usage only
      * 
-     * @param string Message's subject
-     * @param string $text Data to be sent
-     * @param string $notification Default notification message to be pop in administration panel
-     * @return boolean Depending on success
+     * @param string|array $to
+     * @param string $subject
+     * @param string $body
+     * @return \Swift_Message
      */
-    public function send($subject, $text, $notification = 'You have received a new message')
+    private function sendMessage($to, $subject, $body)
     {
         // If we have SMTP transport turned on, then we'd use appropriate Swift's transport
         if ($this->config->getUseSmtpDriver() != true) {
             $from = $this->config->getSmtpUsername();
-
             // SMTP transport
             $transport = \Swift_SmtpTransport::newInstance($this->config->getSmtpHost(), $this->config->getSmtpPort(), $this->config->getSmtpSecureLayer())
                                           ->setUsername($from)
@@ -72,13 +71,39 @@ final class Mailer implements MailerInterface
         $message = \Swift_Message::newInstance($subject)
                               ->setFrom($from)
                               ->setContentType('text/html')
-                              ->setTo(array($this->config->getNotificationEmail()))
-                              ->setBody($text);
+                              ->setTo($to)
+                              ->setBody($body);
         // Re-define the id
-        $msgId = $message->getHeaders()->get('Message-ID');
-        $msgId->setId(time().'.'.uniqid('token').'@'.$this->config->getDomain());
+        //$msgId = $message->getHeaders()->get('Message-ID');
+        //$msgId->setId(time().'.'.uniqid('token').'@'.$this->config->getDomain());
 
-        if ($mailer->send($message, $failed) != 0) {
+        return $mailer->send($message, $failed) != 0;
+    }
+
+    /**
+     * Sends a message to the provided email
+     * 
+     * @param string $email Target email
+     * @param string $subject Email subject
+     * @param string $body
+     * @return boolean
+     */
+    public function sendTo($email, $subject, $body)
+    {
+        return $this->sendMessage(array($email), $subject, $body);
+    }
+
+    /**
+     * Sends a message to administrator
+     * 
+     * @param string Message's subject
+     * @param string $body Body to be sent
+     * @param string $notification Default notification message to be pop in administration panel
+     * @return boolean Depending on success
+     */
+    public function send($subject, $body, $notification = 'You have received a new message')
+    {
+        if ($this->sendMessage(array($this->config->getNotificationEmail()), $subject, $body)) {
             $this->notificationManager->notify($notification);
             return true;
 
--- module/Cms/Service/MailerInterface.php
+++ module/Cms/Service/MailerInterface.php
@@ -14,12 +14,22 @@ namespace Cms\Service;
 interface MailerInterface
 {
     /**
-     * Sends a mail
+     * Sends a message to the provided email
+     * 
+     * @param string $email Target email
+     * @param string $subject Email subject
+     * @param string $body
+     * @return boolean
+     */
+    public function sendTo($email, $subject, $body);
+
+    /**
+     * Sends a message to administrator
      * 
      * @param string Message's subject
-     * @param string $text Data to be sent
+     * @param string $body Body to be sent
      * @param string $notification Default notification message to be pop in administration panel
      * @return boolean Depending on success
      */
-    public function send($subject, $text, $notification = 'You have received a new message');
+    public function send($subject, $body, $notification = 'You have received a new message');
 }
