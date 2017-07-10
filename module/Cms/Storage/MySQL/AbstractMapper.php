653a72bf03f15b09e5cdb03a899575abbcf8d88d
David Yang <daworld.ny@gmail.com>
2017-07-10 16:47:32 +0500

    Added entity translation handlers

--- module/Cms/Storage/MySQL/AbstractMapper.php
+++ module/Cms/Storage/MySQL/AbstractMapper.php
@@ -37,6 +37,127 @@ abstract class AbstractMapper extends BaseMapper
     const PARAM_COLUMN_MODULE = 'module';
 
     /**
+     * Saves the entity
+     * 
+     * @param array $options
+     * @param array $translations
+     * @return boolean
+     */
+    final public function saveEntity(array $options, array $translations)
+    {
+        if (!empty($options[self::PARAM_COLUMN_ID])) {
+            // Update entity
+            $this->db->update(static::getTableName(), $options)
+                     ->whereEquals(self::PARAM_COLUMN_ID, $options[self::PARAM_COLUMN_ID])
+                     ->execute();
+        } else {
+            // ID is incremented automatically, so no need to insert it
+            unset($options[self::PARAM_COLUMN_ID]);
+            // Add entity configuration
+            $this->db->insert(static::getTableName(), $options)
+                     ->execute();
+        }
+
+        // Now handle translations
+        foreach ($translations as $translation) {
+            // Safe type casting
+            $translation[self::PARAM_COLUMN_ID] = (int) $this->getLastId();
+            $translation[self::PARAM_COLUMN_LANG_ID] = (int) $translation[self::PARAM_COLUMN_LANG_ID];
+
+            if ($this->translationExists($translation[self::PARAM_COLUMN_ID], $translation[self::PARAM_COLUMN_LANG_ID])) {
+                // Update translations
+                $this->db->update(static::getTranslationTable(), $translation)
+                         ->whereEquals(self::PARAM_COLUMN_ID, $translation[self::PARAM_COLUMN_ID])
+                         ->andWhereEquals(self::PARAM_COLUMN_LANG_ID, (int) $translation[self::PARAM_COLUMN_LANG_ID])
+                         ->execute();
+            } else {
+                // Insert translation
+                $this->db->insert(static::getTranslationTable(), $translation)
+                         ->execute();
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Create entity select
+     * 
+     * @param array $columns Columns to be selected
+     * @param string $table Table name in case needs to be overridden
+     * @return \Krystal\Db\Db
+     */
+    final protected function createEntitySelect(array $columns, $table = null)
+    {
+        if ($table === null) {
+            $table = static::getTableName();
+        }
+        
+        return $this->db->select($columns)
+                       ->from($table)
+                       // Translation relation
+                       ->leftJoin(static::getTranslationTable())
+                       ->on()
+                       ->equals(
+                            static::getFullColumnName(self::PARAM_COLUMN_ID), 
+                            new RawSqlFragment(static::getFullColumnName(self::PARAM_COLUMN_ID, static::getTranslationTable()))
+                        );
+    }
+
+    /**
+     * Finds an entity
+     * 
+     * @param array $columns Columns to be selected
+     * @param string $id Entity ID
+     * @param boolean $withTranslations Whether to fetch all translations or not
+     * @return array
+     */
+    final protected function findEntity(array $columns, $id, $withTranslations)
+    {
+        $db = $this->createEntitySelect($columns)
+                   ->whereEquals(self::getFullColumnName(self::PARAM_COLUMN_ID), $id);
+
+        if ($withTranslations === true) {
+            return $db->queryAll();
+        } else {
+            return $db->andWhereEquals(self::getFullColumnName(self::PARAM_COLUMN_LANG_ID, static::getTranslationTable()), $this->getLangId())
+                      ->query();
+        }
+    }
+
+    /**
+     * Delete an entity completely
+     * 
+     * @param string|array $id
+     * @return bolean
+     */
+    final public function deleteEntity($id)
+    {
+        if (!is_array($id)) {
+            $id = array($id);
+        }
+
+        $tables = array(
+            static::getTableName(),
+            static::getTranslationTable()
+        );
+
+        // Delete entity with all its relational data
+        return $this->db->delete($tables)
+                     ->from(static::getTableName())
+                     // Translation relation
+                     ->innerJoin(static::getTranslationTable())
+                     ->on()
+                     ->equals(
+                        static::getFullColumnName(self::PARAM_COLUMN_ID), 
+                        new RawSqlFragment(static::getFullColumnName(self::PARAM_COLUMN_ID, static::getTranslationTable()))
+                     )
+                     // Current ID
+                     ->whereIn(static::getFullColumnName(self::PARAM_COLUMN_ID), $id)
+                     ->execute();
+    }
+
+    /**
      * Finds a web page
      * 
      * @param array $columns Columns to be selected
@@ -70,27 +191,19 @@ abstract class AbstractMapper extends BaseMapper
             $table = static::getTableName();
         }
 
-        return $this->db->select($columns)
-                       ->from($table)
-                       // Translation relation
-                       ->leftJoin(static::getTranslationTable())
-                       ->on()
-                       ->equals(
-                            static::getFullColumnName(self::PARAM_COLUMN_ID), 
-                            new RawSqlFragment(static::getFullColumnName(self::PARAM_COLUMN_ID, static::getTranslationTable()))
-                        )
-                        // Web page relation
-                        ->leftJoin(WebPageMapper::getTableName())
-                        ->on()
-                        ->equals(
-                            WebPageMapper::getFullColumnName(self::PARAM_COLUMN_ID),
-                            new RawSqlFragment(static::getFullColumnName(self::PARAM_COLUMN_WEB_PAGE_ID, static::getTranslationTable()))
-                        )
-                        ->rawAnd()
-                        ->equals(
-                            WebPageMapper::getFullColumnName(self::PARAM_COLUMN_LANG_ID),
-                            new RawSqlFragment(static::getFullColumnName(self::PARAM_COLUMN_LANG_ID, static::getTranslationTable()))
-                        );
+        return $this->createEntitySelect($columns, $table)
+                    // Web page relation
+                    ->leftJoin(WebPageMapper::getTableName())
+                    ->on()
+                    ->equals(
+                        WebPageMapper::getFullColumnName(self::PARAM_COLUMN_ID),
+                        new RawSqlFragment(static::getFullColumnName(self::PARAM_COLUMN_WEB_PAGE_ID, static::getTranslationTable()))
+                    )
+                    ->rawAnd()
+                    ->equals(
+                        WebPageMapper::getFullColumnName(self::PARAM_COLUMN_LANG_ID),
+                        new RawSqlFragment(static::getFullColumnName(self::PARAM_COLUMN_LANG_ID, static::getTranslationTable()))
+                    );
     }
 
     /**
