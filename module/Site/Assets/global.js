651cf7b299d56aa036d230ebdfe8c26c7ee12107
dave-yang <daworld.ny@gmail.com>
2019-01-20 20:44:56 +0500

    Avoid conflicts with Jquery

--- module/Site/Assets/global.js
+++ module/Site/Assets/global.js
@@ -7,279 +7,281 @@ if (!(window.jQuery)){
     throw new Error('Jquery is not loaded. Halting execution');
 }
 
-$(function(){
-    /**
-     * Validator class constructor
-     * 
-     * @param $form Jquey form object
-     * @return void
-     */
-    function Validator($form){
-        this.$form = $form;
-    }
-
-    Validator.prototype = {
+(function($){
+    $(function(){
         /**
-         * Builds a selector for a target element
+         * Validator class constructor
          * 
-         * @param string name Element's name
-         * @return string Built selector
-         */
-        buildElementSelector : function(name){
-            return selector = '[name="' + name + '"]';
-        },
-
-        /**
-         * Finds container element by a child's name inside it
-         * 
-         * @param string name Child element's name
-         * @return object
-         */
-        getContainerElementByClosestName : function(name){
-            var selector = this.buildElementSelector(name);
-            return this.$form.find(selector).closest('div.form-group');
-        },
-
-        /**
-         * Returns parent element's container
-         * 
-         * @param string name Child element's name
-         * @return object
-         */
-        getParentContainer : function(name){
-            var selector = this.buildElementSelector(name);
-            return $(selector).parent();
-        },
-
-        /**
-         * Creates message block
-         * 
-         * @param string text Text to be appeared within container
-         * @return object
-         */
-        createMessageElement : function(text){
-            var element = document.createElement('span');
-
-            // Configure element for bootstrap
-            $span = $(element).attr('class', 'help-block')
-                              .text(text);
-
-            return $span;
-        },
-
-        /**
-         * Checks whether parent element has a helper block
-         * 
-         * @param object $container
-         * @return boolean
-         */
-        hasHelpBlock : function($container){
-            return $container.length > 0;
-        },
-
-        /**
-         * Checks whether element represents a collection of values
-         * 
-         * @param string name Element name
-         * @return boolean
-         */
-        isArrayElement: function(name){
-            // Notation to be looked up
-            var arrayNotation = '[]';
-
-            // Try to build an element appending array notation first
-            var $element = $(this.buildElementSelector(name + arrayNotation));
-
-            if ($element.length) {
-                return $element.attr('name').indexOf(arrayNotation) !== -1;
-            } else {
-                // Not array element
-                return false;
-            }
-        },
-
-        /**
-         * Checks whether element is radio by its type
-         * 
-         * @param string name Element name
-         * @return boolean
-         */
-        isRadioElement: function(name){
-            var $element = $(this.buildElementSelector(name));
-            var type = $element.attr('type');
-
-            return type === 'radio';
-        },
-        
-        /**
-         * Shows an error which belongs to a field
-         * 
-         * @param string name Element's name
-         * @param string message To be appended
+         * @param $form Jquey form object
          * @return void
          */
-        showErrorOn : function(name, message){
-            var isArrayElement = this.isArrayElement(name);
-            var isRadioElement = this.isRadioElement(name);
+        function Validator($form){
+            this.$form = $form;
+        }
 
-            // If this one looks as a collection input, then append array notation to its name
-            if (isArrayElement) {
-                name += '[]';
-            }
+        Validator.prototype = {
+            /**
+             * Builds a selector for a target element
+             * 
+             * @param string name Element's name
+             * @return string Built selector
+             */
+            buildElementSelector : function(name){
+                return selector = '[name="' + name + '"]';
+            },
 
-            $container = this.getContainerElementByClosestName(name);
+            /**
+             * Finds container element by a child's name inside it
+             * 
+             * @param string name Child element's name
+             * @return object
+             */
+            getContainerElementByClosestName : function(name){
+                var selector = this.buildElementSelector(name);
+                return this.$form.find(selector).closest('div.form-group');
+            },
 
-            if ($container.hasClass('has-success')) {
-                $container.removeClass('has-success');
-            }
+            /**
+             * Returns parent element's container
+             * 
+             * @param string name Child element's name
+             * @return object
+             */
+            getParentContainer : function(name){
+                var selector = this.buildElementSelector(name);
+                return $(selector).parent();
+            },
 
-            $container.addClass('has-error');
+            /**
+             * Creates message block
+             * 
+             * @param string text Text to be appeared within container
+             * @return object
+             */
+            createMessageElement : function(text){
+                var element = document.createElement('span');
 
-            // Don't show errors on radio and array elements
-            if (!isRadioElement && !isArrayElement) {
-                $span = this.createMessageElement(message);
+                // Configure element for bootstrap
+                $span = $(element).attr('class', 'help-block')
+                                  .text(text);
 
-                $parent = this.getParentContainer(name);
-                $parent.append($span);
-            }
-        },
+                return $span;
+            },
 
-        /**
-         * Resets control elements to their initial state
-         * 
-         * @return void
-         */
-        resetAll : function(){
-            // Classes we'd like to remove when resetting all
-            var classes = ['has-error', 'has-warning', 'has-success'];
+            /**
+             * Checks whether parent element has a helper block
+             * 
+             * @param object $container
+             * @return boolean
+             */
+            hasHelpBlock : function($container){
+                return $container.length > 0;
+            },
 
-            this.$form.find('div.form-group').each(function(){
-                for (var key in classes) {
-                    // Value represents class name
-                    var value = classes[key];
+            /**
+             * Checks whether element represents a collection of values
+             * 
+             * @param string name Element name
+             * @return boolean
+             */
+            isArrayElement: function(name){
+                // Notation to be looked up
+                var arrayNotation = '[]';
 
-                    if ($(this).hasClass(value)) {
-                        $(this).removeClass(value);
-                    }
-                }
-            });
+                // Try to build an element appending array notation first
+                var $element = $(this.buildElementSelector(name + arrayNotation));
 
-            // Now we'd assume that everything is okay, and later remove this class on demand
-            this.$form.find('div.form-group').addClass('has-success');
-
-            // Remove all helper spans
-            this.$form.find("span.help-block").remove();
-        },
-
-        /**
-         * Shows error messages
-         * 
-         * @param string response Server's response
-         * @param undefined|string backUrl
-         * @return void
-         */
-        handleAll : function(response, backUrl){
-            // Clear all previous messages and added classes
-            this.resetAll();
-
-            // if its not JSON, but "1" then we'd assume success
-            if (response == "1") {
-                // If its provided, then do redirect to that URL
-                if (backUrl){
-                    window.location = backUrl;
+                if ($element.length) {
+                    return $element.attr('name').indexOf(arrayNotation) !== -1;
                 } else {
-                    // Otherwise, just reload the page
-                    window.location.reload();
+                    // Not array element
+                    return false;
+                }
+            },
+
+            /**
+             * Checks whether element is radio by its type
+             * 
+             * @param string name Element name
+             * @return boolean
+             */
+            isRadioElement: function(name){
+                var $element = $(this.buildElementSelector(name));
+                var type = $element.attr('type');
+
+                return type === 'radio';
+            },
+            
+            /**
+             * Shows an error which belongs to a field
+             * 
+             * @param string name Element's name
+             * @param string message To be appended
+             * @return void
+             */
+            showErrorOn : function(name, message){
+                var isArrayElement = this.isArrayElement(name);
+                var isRadioElement = this.isRadioElement(name);
+
+                // If this one looks as a collection input, then append array notation to its name
+                if (isArrayElement) {
+                    name += '[]';
                 }
 
-            } else {
-                // Otherwise, try to handle JSON data
-                try {
-                    var data = $.parseJSON(response);
+                $container = this.getContainerElementByClosestName(name);
 
-                    for (var name in data){
-                        var message = data[name];
-                        this.showErrorOn(name, message);
+                if ($container.hasClass('has-success')) {
+                    $container.removeClass('has-success');
+                }
+
+                $container.addClass('has-error');
+
+                // Don't show errors on radio and array elements
+                if (!isRadioElement && !isArrayElement) {
+                    $span = this.createMessageElement(message);
+
+                    $parent = this.getParentContainer(name);
+                    $parent.append($span);
+                }
+            },
+
+            /**
+             * Resets control elements to their initial state
+             * 
+             * @return void
+             */
+            resetAll : function(){
+                // Classes we'd like to remove when resetting all
+                var classes = ['has-error', 'has-warning', 'has-success'];
+
+                this.$form.find('div.form-group').each(function(){
+                    for (var key in classes) {
+                        // Value represents class name
+                        var value = classes[key];
+
+                        if ($(this).hasClass(value)) {
+                            $(this).removeClass(value);
+                        }
+                    }
+                });
+
+                // Now we'd assume that everything is okay, and later remove this class on demand
+                this.$form.find('div.form-group').addClass('has-success');
+
+                // Remove all helper spans
+                this.$form.find("span.help-block").remove();
+            },
+
+            /**
+             * Shows error messages
+             * 
+             * @param string response Server's response
+             * @param undefined|string backUrl
+             * @return void
+             */
+            handleAll : function(response, backUrl){
+                // Clear all previous messages and added classes
+                this.resetAll();
+
+                // if its not JSON, but "1" then we'd assume success
+                if (response == "1") {
+                    // If its provided, then do redirect to that URL
+                    if (backUrl){
+                        window.location = backUrl;
+                    } else {
+                        // Otherwise, just reload the page
+                        window.location.reload();
                     }
 
-                } catch(e) {
-                    // Otherwise we'd assume that something went wrong
-                    console.log(response);
+                } else {
+                    // Otherwise, try to handle JSON data
+                    try {
+                        var data = $.parseJSON(response);
+
+                        for (var name in data){
+                            var message = data[name];
+                            this.showErrorOn(name, message);
+                        }
+
+                    } catch(e) {
+                        // Otherwise we'd assume that something went wrong
+                        console.log(response);
+                    }
                 }
             }
-        }
-    };
+        };
 
-    /**
-     * Global factory for form validator
-     * 
-     * @param object $form Jquery form object
-     * @return Validator
-     */
-    $.getValidator = function($form){
-        return new Validator($form);
-    };
-    
-    // Setup global AJAX settings
-    $.ajaxSetup({
-        cache : false,
-        charset : "UTF-8",
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        },
-        beforeSend : function(){
-            // Ensure bootstrap modal is loaded
-            if ($.isFunction($.fn.modal)) {
-                $("#ajax-modal").modal('show');
+        /**
+         * Global factory for form validator
+         * 
+         * @param object $form Jquery form object
+         * @return Validator
+         */
+        $.getValidator = function($form){
+            return new Validator($form);
+        };
+        
+        // Setup global AJAX settings
+        $.ajaxSetup({
+            cache : false,
+            charset : "UTF-8",
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            beforeSend : function(){
+                // Ensure bootstrap modal is loaded
+                if ($.isFunction($.fn.modal)) {
+                    $("#ajax-modal").modal('show');
+                }
+            },
+            complete : function(){
+                // Ensure bootstrap modal is loaded
+                if ($.isFunction($.fn.modal)) {
+                    $("#ajax-modal").modal('hide');
+                }
             }
-        },
-        complete : function(){
-            // Ensure bootstrap modal is loaded
-            if ($.isFunction($.fn.modal)) {
-                $("#ajax-modal").modal('hide');
-            }
-        }
-    });
+        });
 
-    // CAPTCHA's button
-    $("[data-captcha='button-refresh']").click(function(event){
-        event.preventDefault();
-
-        // Grab image's element
-        var $image = $("[data-captcha='image']");
-        var link = $image.attr('src');
-
-        $image.attr('src', link + Math.random());
-    });
-
-    // For forms that send data
-    $("[data-button='submit']").click(function(){
-        // Find its parent form
-        var $form = $(this).closest('form');
-
-        $form.off('submit').submit(function(event){
+        // CAPTCHA's button
+        $("[data-captcha='button-refresh']").click(function(event){
             event.preventDefault();
 
-            var $self = $(this);
-            var url = $self.attr('action') ? $self.attr('action') : '/';
-            var method = $self.attr('method') ? $self.attr('method') : 'POST';
+            // Grab image's element
+            var $image = $("[data-captcha='image']");
+            var link = $image.attr('src');
 
-            $.ajax({
-                url: url,
-                contentType: false,
-                processData: false,
-                data: new FormData($(this)[0]),
-                type: method,
-                success: function(response){
-                    $.getValidator($form).handleAll(response, $self.data('back-url'));
-                }
+            $image.attr('src', link + Math.random());
+        });
+
+        // For forms that send data
+        $("[data-button='submit']").click(function(){
+            // Find its parent form
+            var $form = $(this).closest('form');
+
+            $form.off('submit').submit(function(event){
+                event.preventDefault();
+
+                var $self = $(this);
+                var url = $self.attr('action') ? $self.attr('action') : '/';
+                var method = $self.attr('method') ? $self.attr('method') : 'POST';
+
+                $.ajax({
+                    url: url,
+                    contentType: false,
+                    processData: false,
+                    data: new FormData($(this)[0]),
+                    type: method,
+                    success: function(response){
+                        $.getValidator($form).handleAll(response, $self.data('back-url'));
+                    }
+                });
             });
+        });
+
+        // Work flawlessly with <BASE> tag
+        $("a[href^='\#']").each(function(){ 
+            this.href = location.href.split("#")[0] + '#' + this.href.substr(this.href.indexOf('#') + 1);
         });
     });
 
-    // Work flawlessly with <BASE> tag
-    $("a[href^='\#']").each(function(){ 
-        this.href = location.href.split("#")[0] + '#' + this.href.substr(this.href.indexOf('#') + 1);
-    });
-    
-});
+})(jQuery);
