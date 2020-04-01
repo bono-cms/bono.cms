642e1fc4d879e9f185188a1637292d1cab6fa9e7
dave-yang <daworld.ny@gmail.com>
2020-04-02 01:34:55 +0500

    Removed legacy sitemap stuff

--- module/Cms/Controller/Admin/SitemapLinks.php
+++ module/Cms/Controller/Admin/SitemapLinks.php
@@ -28,7 +28,6 @@ final class SitemapLinks extends AbstractController
                    ->addOne('Sitemap links');
 
         return $this->view->render('sitemap-links', array(
-            'links' => $this->createLinks(),
             'priorities' => SitemapTool::getPriorities(),
             'changefreqs' => SitemapTool::getChangefreqs(),            
             'priority' => $config->getSitemapPriority(),
@@ -56,35 +55,15 @@ final class SitemapLinks extends AbstractController
      */
     public function pingAction()
     {
-        if (SitemapTool::ping($this->createLinks())) {
+        // Front SiteMap URL
+        $url = $this->request->getBaseUrl().$this->createUrl('Site:Sitemap@indexAction');
+
+        if (SitemapTool::ping($url)) {
             $this->flashBag->set('success', 'Search engines have been informed about new version of your sitemap!');
         } else {
             $this->flashBag->set('warning', 'There was a connection error while submitting the sitemap');
         }
 
         return '1';
-    }
-
-    /**
-     * Create sitemap URL links
-     * 
-     * @return array
-     */
-    private function createLinks()
-    {
-        $links = array();
-        $languages = $this->getModuleService('languageManager')->fetchAll(true);
-
-        if (count($languages) > 1) {
-            foreach ($languages as $language) {
-                $url = $this->request->getBaseUrl().$this->createUrl('Site:Main@sitemapAction', array($language->getCode()), 1);
-                $links[$language->getName()]= $url;
-            }
-
-        } else {
-            $links[$languages[0]->getName()] = $this->request->getBaseUrl().$this->createUrl('Site:Main@sitemapAction');
-        }
-
-        return $links;
     }
 }
