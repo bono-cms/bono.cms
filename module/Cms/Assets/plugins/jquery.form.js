614264e6567fd7a6b265896430c6e3fab1d90684
dave-yang <daworld.ny@gmail.com>
2024-03-31 00:36:13 +0500

    Removed extra spaces

--- module/Cms/Assets/plugins/jquery.form.js
+++ module/Cms/Assets/plugins/jquery.form.js
@@ -1,10 +1,7 @@
-
 /**
  * Small jquery plugin to simplify sending forms
  */
-
 (function($){
-	
 	/**
 	 * Sends a form, including files
 	 * 
@@ -13,29 +10,32 @@
 	 */
 	$.fn.send = function(options){
 		if (options.url == undefined){
-			options.url = '';
+            options.url = '';
 		}
-		
+
 		if ($(this)[0].tagName.toLowerCase() != "form"){
-			throw new Error('You can not send anything but form!');
+            throw new Error('You can not send anything but form!');
 		}
-		
+
 		$(this).off('submit').submit(function(event){
 			event.preventDefault();
 			var data = new FormData($(this)[0]);
-			
+
 			if (typeof options.before != undefined && $.isFunction(options.before)) {
 				options.before();
 			}
-			
+
 			$.ajax({
-				type : "POST",
-				cache : false,
-				url : options.url,
-				processData : false,
-				contentType : false,
-				data : data,
-				success : function(response){
+				type: "POST",
+				cache: false,
+				url: options.url,
+				processData: false,
+				contentType: false,
+				data: data,
+                error: function(jqXHR, textStatus, errorThrown){
+                    console.log(textStatus + ' : ' + errorThrown);
+                },
+				success: function(response){
 					if (typeof options.success != undefined && $.isFunction(options.success)) {
 						options.success(response);
 					}
@@ -43,5 +43,5 @@
 			});
 		});
 	};
-	
+
 })(jQuery);
