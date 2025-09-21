13a304d3885f0a1f8a957805fee4c32c78538cd3
dave-yang <daworld.ny@gmail.com>
2025-09-21 19:54:17 +0500

    Fix indentation in admin config.js
    Corrected inconsistent indentation in the email configuration batch handler for improved code readability and maintainability.

--- module/Cms/Assets/admin/config.js
+++ module/Cms/Assets/admin/config.js
@@ -1,10 +1,10 @@
 $(function(){
 	var handle = function(state){
-		// Do the batch for email elements
-		$("[name='config[smtp_secure_layer]'], [name='config[smtp_host]'], [name='config[smtp_username]'], [name='config[smtp_password]'], [name='config[smtp_port]']")
-		.prop('disabled', state);
+        // Do the batch for email elements
+        $("[name='config[smtp_secure_layer]'], [name='config[smtp_host]'], [name='config[smtp_username]'], [name='config[smtp_password]'], [name='config[smtp_port]']")
+        .prop('disabled', state);
 	};
-	
+
 	var $useDriver = $("[name='config[use_smtp_driver]']");
 	handle($useDriver.is(':checked'));
 
